8d206db6e05127a7bea41444a3a7e0e9e1c92450
imcnanie <ianmcnanie@gmail.com>
2022-07-12 21:28:22 -0700

    Add files via upload

--- src/blog/gen_blog.py
+++ src/blog/gen_blog.py
@@ -7,7 +7,32 @@ class blogGen:
     def __init__(self):
         blogs = glob.glob("./*/main.md")
 
-        lines = ''
+        lines = '''
+# Title
+Blog
+        
+# Style
+img {
+padding: 10px;
+margin: 5px;
+background-color: {color_accent_light}; 
+}
+
+.img_div {
+padding: 10px;
+margin: 5px;
+background-color: {color_accent_light}; 
+}
+        
+
+.scroll {
+font-size: medium; 
+line-height: 1.6;
+}
+
+# HTML
+'''
+        
         base = "\"https://monksevillair.com/src/blog"
         
         for b in sorted(blogs)[::-1]:
@@ -22,7 +47,7 @@ class blogGen:
                     ll = lll.replace("\"./",base+b.strip("main.md"))
                     l = ll.replace("\"~/","https://monksevillair.com/")
                     lines += l.strip("\r").strip("\n") + "  \r\n"
-
+                    
                 lines += "\r\n"+ "---  "+ "\r\n\r\n"
                 
         #print(lines)
