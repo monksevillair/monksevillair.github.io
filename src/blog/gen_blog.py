f2e373670d222100c954344d75c4e91cbc990972
imcnanie <ianmcnanie@gmail.com>
2022-07-12 22:47:44 -0700

    Add files via upload

--- src/blog/gen_blog.py
+++ src/blog/gen_blog.py
@@ -34,13 +34,13 @@ line-height: 1.6;
 '''
         
         base = "\"https://monksevillair.com/src/blog"
-        base2 = "\"https://monksevillair.com/blog"
+        base2 = "https://monksevillair.com/blog"
         
         for b in sorted(blogs)[::-1]:
             with open(b) as f:
                 date = dparser.parse(b,fuzzy=True)
                 title = b.split(str(date.strftime("%Y-%m-%d")))[-1].strip("/main.md").strip("/")[1::].replace("-", " ")
-                lines += "[{}](## ".format(base2+b[1::].replace("md","html"))+ title + ")  \r\n"
+                lines += "[## {title}]({link})  \r\n".format(link=base2+b[1::].replace("md","html"), title=title)
                 lines += "### "+ str(date.strftime("%A, %B %d %Y")) + "  \r\n"
 
                 for lll in f.readlines():
